69b2535e0a3f0b7131220fa37fc61bee761bb0d6
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-04-28 02:05:27 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World"
+  end
+
+  get '/name'do
+    "My name is Cameron"
+  end
+
+  get '/hometown'do
+      "My hometown is Placerville"
+  end
+
+  get '/favorite-song'do
+      "My favorite song is Moral Of the Story"
+  end
 end
